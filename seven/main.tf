6f9ca5829aae6393666678d3c80403e918a39f3f
Jacob McSwain <jacob.a.mcswain@gmail.com>
2023-12-14 15:08:21 -0600

    make subdirs

---
+++ seven/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
+  }
+}
+
+resource "null_resource" "test" {
+  triggers = {
+    always_run = var.always_run
+  }
+}
+
+variable "always_run" {
+  type    = number
+  default = 0
+}
